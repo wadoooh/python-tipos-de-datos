a7deab9d39d18ce32820c0610a4810de9d17cc45
Daniel Sanchez <daniel.sanchez@apriorins.com>
2023-08-10 00:48:54 -0500

    Se lee el valor de la operación por consola. Se ejecuta el tipo de operación por medio de comparaciones con if sencillos.

--- Calculadora.py
+++ Calculadora.py
@@ -3,19 +3,22 @@ print("Ingrese el número 1")
 numero1 = int(input())
 print("Ingrese el número 2")
 numero2 = int(input())
+print("Ingrese la operación (+, -, *, /)")
+operacion = input()
 
-# Operación suma:
-suma = numero1 + numero2
-print("La suma es " + str(suma))
-
-# Operación resta:
-resta = numero1 - numero2
-print("La resta es " + str(resta))
-
-# Operación multiplicación:
-multiplicacion = numero1 * numero2
-print("La multiplicación es " + str(multiplicacion))
-
-# Operación división:
-division = numero1 / numero2
-print("La división es " + str(division))
+if operacion == '+':
+    # Operación suma:
+    suma = numero1 + numero2
+    print("La suma es " + str(suma))
+if operacion == '-':
+    # Operación resta:
+    resta = numero1 - numero2
+    print("La resta es " + str(resta))
+if operacion == '*':
+    # Operación multiplicación:
+    multiplicacion = numero1 * numero2
+    print("La multiplicación es " + str(multiplicacion))
+if operacion == '/':
+    # Operación división:
+    division = numero1 / numero2
+    print("La división es " + str(division))
